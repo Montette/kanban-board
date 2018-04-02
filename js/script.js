40c4667185646a8d60cdf13eaa99a332bc139778
Monika Radosiewicz <monika.radosiewicz@gmail.com>
2018-04-02 15:20:53 +0200

    add basic html

---
+++ js/script.js
@@ -0,0 +1,56 @@
+$(function() {
+    
+    
+   function randomString(){
+       var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
+       var str = '';
+       for (var i = 0; i < 10; i++){
+           str+=chars[Math.floor(Math.random()*chars.length)];
+       }
+       return str;
+   } 
+    
+    function Column(name){
+        var self = this;
+        this.id = randomString();
+        this.name = name;
+        this.$element = createColumn();
+        function createColumn(){
+            
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+})
